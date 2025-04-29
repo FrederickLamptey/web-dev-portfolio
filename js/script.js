1198b6b9ee99b4190ce46ba5631af4ee33d9f10e
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-04-29 18:47:25 -0400

    write mobile media query

--- js/script.js
+++ js/script.js
@@ -14,20 +14,38 @@ document.querySelector('.nav-list').addEventListener('click', function (e) {
 const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) return;
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
 
-      entry.target.classList.remove('section--hidden');
-      observer.unobserve(entry.target);
-    });
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.10,
+  threshold: 0.1,
 });
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+//hamburger menu
+// const menuToggle = document.querySelector('.menu-toggle');
+// const navList = document.querySelector('.nav-list');
+
+// menuToggle.addEventListener('click', () => {
+//   navList.classList.toggle('open');
+// });
+
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function closeSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
